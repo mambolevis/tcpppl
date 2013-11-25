603691d3a3190733bc84bc0657a5226cf74273af
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2013-11-25 14:57:47 -0600

    generalize to N threads

--- test/task.cpp
+++ test/task.cpp
@@ -6,42 +6,135 @@
 
 #include <future>
 #include <iostream>
+#include <memory>
 #include <numeric>
 #include <thread>
+#include <vector>
 
 using std::cout;
 using std::endl;
 using std::future;
 using std::packaged_task;
 using std::thread;
+using std::unique_ptr;
+using std::vector;
 
-int sum(const int *from, const int &elements, int init)
-{
-    return std::accumulate(from, from + elements, init);
-}
+template<typename T>
+    T sum(const T *from, const T &number_of_elements, T init)
+        // calculate a sum of consequent elements
+    {
+        return std::accumulate(from, from + number_of_elements, init);
+    }
+
+template<typename Fn, typename R>
+    class Task
+        // wrapper for a task, future and thread
+    {
+        public:
+            // create task for given function
+            //
+            // the future is automatically extracted from the task
+            //
+            Task(Fn &&function):
+                _task{&function},
+                _future{_task.get_future()}
+            {}
+
+            // Prevent copy
+            //
+            Task(const Task &) = delete;
+            Task &operator =(const Task &) = delete;
+            
+            // Prevent move
+            //
+            Task(Task &&) = delete;
+            Task &operator =(Task &&) = delete;
+
+            template<typename... Tail>
+                void run(Tail&&... tail)
+                    // run task with arguments if there are any
+                {
+                    if (!_thread)
+                        _thread.reset(new thread{std::move(_task), tail...});
+                }
+
+            void join()
+                // let thread finish its job
+            {
+                if (_thread)
+                    _thread->join();
+            } 
+
+            R get()
+            {
+                return _future.get();
+            }
+
+        private:
+            packaged_task<Fn> _task;    // task for given function
+            future<R> _future;          // task's future
+            unique_ptr<thread> _thread; // thread where task will be run
+    };
 
 int main(int argc, char *argv[])
 {
-    const int size {100000};
-    int collection[size];
-    for(int i {0}; size > i; ++i)
+    const long size {10000};  // array size
+    const int threads_num {7};  // number of threads to be created
+
+    // fill array with data (linear function)
+    //
+    long collection[size];
+    for(long i {0}; size > i; ++i)
         collection[i] = i + 1;
 
-    using Task = int(const int *, const int &, int);
+    // alias to Task and pointer: tired of typing
+    //
+    using ITask = Task<long(const long *, const long &, long),
+                       long>;
+    using ITaskP = unique_ptr<ITask>;
 
-    packaged_task<Task> task1 {&sum};
-    packaged_task<Task> task2 {&sum};
+    // create threads
+    //
+    vector<ITaskP> threads;
+    for(int i {0}; threads_num > i; ++i)
+        threads.push_back(ITaskP{new ITask{sum}});
 
-    future<int> f1 {task1.get_future()};
-    future<int> f2 {task2.get_future()};
+    // run threads
+    //
+    { 
+        // number of elements to be processed by each thread but last
+        //
+        const unsigned long delta {size / threads.size()}; 
+        int i {0};
+        for(auto &t:threads)
+        {
+            t->run(collection + i * delta,
+                   &t == &threads.back() ?  size - i * delta : delta,
+                   0);
 
-    const int half_size {size / 2};
-    thread t1 {std::move(task1), collection, half_size, 0};
-    thread t2 {std::move(task2), collection + half_size, half_size, 0};
+            ++i;
+        }
+    }
 
-    cout << "sum: " << size * (size + 1) / 2 <<  endl;
-    cout << "sum (threads): " << f1.get() + f2.get() << endl;
+    // calculate sum of results
+    //
+    long sum = 0;
+    for(auto &t:threads)
+        sum += t->get();
 
-    t1.join();
-    t2.join();
+    // print the expected and calculated results; compare the two
+    //
+    const long expected_sum {size * (size + 1) / 2};
+
+    cout << " expected sum: " << expected_sum << endl;
+    cout << "sum (threads): " << sum << endl;
+
+    cout << "validation: " << (expected_sum == sum ? "pass" : "fail") << endl;
+
+    // let each thread cleanly exit
+    //
+    // technically threads should finish their task by this moment
+    //
+    for(auto &t:threads)
+        t->join();
 }
