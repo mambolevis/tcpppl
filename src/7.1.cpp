0f106020acbedc80619f7f57e431e7977fe1d17a
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2013-11-27 19:18:27 -0600

    complete Hello World exercise

---
+++ src/7.1.cpp
@@ -0,0 +1,19 @@
+// Created by Samvel Khalatyan on Nov 27, 2013
+// Copyright (c) 2013 Samvel Khalatyan. All rights reserved
+//
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+//
+// (*2) Get the ‘‘Hello, world!’’ program (§2.2.1) to run. This is not an
+//      exercise in programming. It is an exercise to test your use of your
+//      edit-compile-link-execute tool chain.
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(int, char *[])
+{
+    cout << "Hello, world!" << endl;
+}
