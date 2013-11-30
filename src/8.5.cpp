1c99d1ea79c6ecf073c3efd70129124378fac9d9
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2013-11-30 16:47:23 -0600

    print number of ints between elements of array of ints

---
+++ src/8.5.cpp
@@ -0,0 +1,16 @@
+// Created by Samvel Khalatyan on Nov 30, 2013
+// Copyright (c) 2013 Samvel Khalatyan. All rights reserved
+//
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+//
+// (*1) Given two int*'s pointing into an array, find and output the number of
+//      ints between the two pointed-to ints (zero if they point to the same
+//      element).
+
+#include "interface/8.4.h"
+
+int main(int, char *[])
+{
+    ex_8_4::run({1, 2, 3, 4, 5});
+}
