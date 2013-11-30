772a4ace65139837ebd3d53cdec23a5f099b0564
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2013-11-30 16:41:59 -0600

    move run into separate function for reuse

--- src/8.4.cpp
+++ src/8.4.cpp
@@ -10,35 +10,48 @@
 
 #include <chrono>
 #include <cmath>
+#include <initializer_list>
 #include <iostream>
 #include <random>
 
 using std::cout;
 using std::endl;
 
-void *cast(char *p)
-{
-    return static_cast<void *>(p);
-}
+template<typename T>
+    void *cast(T *p)
+    {
+        return static_cast<void *>(p);
+    }
+
+template<typename T>
+    void *cast(const T *p)
+    {
+        return static_cast<void *>(const_cast<T *>(p));
+    }
+
+template<typename T>
+    void run(std::initializer_list<T> &&list)
+    {
+        std::default_random_engine e{
+            std::chrono::system_clock::now().time_since_epoch().count()};
+        std::uniform_int_distribution<int> d{0, list.size() - 1};
+        auto index = std::bind(d, e);
+
+        for(int i {0}; 10 > i; ++i)
+        {
+            int i1 = index();
+            int i2 = index();
+
+            const T *p1 = &*list.begin() + i1;
+            const T *p2 = &*list.begin() + i2;
+
+            cout << '[' << i1 << "]-[" << i2 << "] == " <<
+                cast(p1) << " - " << cast(p2) <<
+                " = " << std::abs(p1 - p2) << endl;
+        }
+    }
 
 int main(int, char *[])
 {
-    std::default_random_engine e{
-        std::chrono::system_clock::now().time_since_epoch().count()};
-    std::uniform_int_distribution<int> d{0, 9};
-    auto index = std::bind(d, e);
-
-    char array[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
-    for(int i {0}; 5 > i; ++i)
-    {
-        int i1 = index();
-        int i2 = index();
-
-        char *p1 = array + i1;
-        char *p2 = array + i2;
-
-        cout << '[' << i1 << "]-[" << i2 << "] == " <<
-            cast(p1) << " - " << cast(p2) <<
-            " = " << std::abs(p1 - p2) << " chars" << endl;
-    }
+    run({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'});
 }
