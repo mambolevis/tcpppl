dd24b938b7e0e22bd45ea3940a028705be21e88f
Samvel Khalatyan <samvel.khalatyan@gmail.com>
2013-12-04 17:55:38 -0600

    comment atoi

--- src/10.4.cpp
+++ src/10.4.cpp
@@ -11,15 +11,19 @@
 //      addition to plain decimal numbers.
 //      Modify atoi() to handle the C++ character constant notation.
 
+#include <initializer_list>
 #include <iomanip>
 #include <iostream>
 #include <vector>
 #include <utility>
 
 int atoi(const char *str)
+    // convert string to integer
+    //
+    // function supports positive/negative numbers
+    // it also supports dec, oct, and hex bases
 {
-    int base {10};
-    char max_char {'9'};
+    // Get value sign
     int sign {1};
     if ('-' == *str)
     {
@@ -29,6 +33,9 @@ int atoi(const char *str)
     else if ('+' == *str)
         ++str;
 
+    // detect base
+    int base {10};
+    char max_char {'9'}; // max char used in the base
     if ('0' == *str && 'x' == *(str + 1))
     {
         str += 2;   // skip prefix '0x'
@@ -42,6 +49,7 @@ int atoi(const char *str)
         base = 8;
     }
 
+    // extract the value
     int result {0};
     while(*str)
     {
@@ -63,6 +71,7 @@ int atoi(const char *str)
 }
 
 int atoi(const char &c)
+    // convert single character to int
 {
     char str[2] {c, '\0'};
     return atoi(str);
@@ -71,28 +80,39 @@ int atoi(const char &c)
 int main(int, char *[])
 {
     using namespace std;
-    
-    using test = pair<int, initializer_list<string>>;
+
+    // each test is a pair of {base, {test strings}}
+    using test = pair<int, initializer_list<const char *>>;
     vector<test> tests {
-        {10, {"123", "432", "692", "+123", "-123"}},
-        {8, {"0123", "0432", "0692", "0010101", "04751", "-0123"}},
-        {16, {"0x123", "0x432", "0x692", "0xa3d", "0xff", "-0xff"}}
+        {10, {"123", "432", "692",
+              "+123",
+              "-123"}},
+
+        {8, {"0123", "0432", "0692", "0010101", "04751",
+             "-0123"}},
+
+        {16, {"0x123", "0x432", "0x692", "0xa3d", "0xff",
+              "-0xff"}}
     };
 
+    // run tests with strings
     for(const auto &t:tests)
     {
         cout << "-- base " << t.first << endl;
         for(const auto &s:t.second)
         {
-            ios_base::fmtflags flags = cout.flags();
-            const int value = atoi(s.c_str());
-            cout << setw(10) << s << " | " << setw(10) << left << value <<
-                " | ";
-            cout << showbase << setbase(t.first) << abs(value) << endl;
-            cout.flags(flags);
+            const int value = atoi(s);
+
+            ios_base::fmtflags flags = cout.flags(); // keep flags for restore
+            cout << setw(10) << s <<
+                " | " << setw(10) << left << value <<
+                " | " << showbase << setbase(t.first) << abs(value) << endl;
+
+            cout.flags(flags); // restore flags
         }
     }
 
+    // run tests with chars
     cout << "-- base 10" << endl;
     for(const auto &c:{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'})
     {
